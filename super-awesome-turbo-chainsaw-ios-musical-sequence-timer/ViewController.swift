617a961a70b442e334bc902f8fe00939622d9e03
Jeremy J Hartmann <jhartman@sfu.ca>
2016-07-19 12:24:44 -0700

    Initial Commit

---
+++ super-awesome-turbo-chainsaw-ios-musical-sequence-timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  super-awesome-turbo-chainsaw-ios-musical-sequence-timer
+//
+//  Created by Jeremy Hartmann on 2016-07-19.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
